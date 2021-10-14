819fcf31f9bbe5e9414baed638c653a84f6e093e
Roy Shemesh <89579074+RoyShemesh@users.noreply.github.com>
2021-10-14 15:52:19 +0300

    HW

---
+++ 05.AJAX/src/index.js
@@ -0,0 +1,11 @@
+
+const baseUrl="https://pokeapi.co/api/v2/pokemon/";
+ getPokemonByName();
+const getPokemonByName= async ()=>{
+    console.log(respone); 
+    try {
+        const respone =await axios.get(baseUrl);
+    } catch (error) {
+        
+    }
+}
