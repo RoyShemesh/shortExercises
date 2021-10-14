e9482ac77a8c6b0f43dc3c9ff5c584b8b4046cd7
Roy Shemesh <89579074+RoyShemesh@users.noreply.github.com>
2021-10-14 16:01:03 +0300

    hw

--- 05.AJAX/src/index.js
+++ 05.AJAX/src/index.js
@@ -1,10 +1,10 @@
 
 const baseUrl="https://pokeapi.co/api/v2/pokemon/";
- getPokemonByName();
+getPokemonByName();
 const getPokemonByName= async ()=>{
-    console.log(respone); 
     try {
         const respone =await axios.get(baseUrl);
+        console.log(respone);
     } catch (error) {
         
     }
